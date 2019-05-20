af0321be597c783e6db96e4a192f4118773ca98e
Srinu <nsrinu@ms.ds.uhc.com>
2019-05-20 17:21:14 +0530

    Adding MicroServices Startup

---
+++ limits-service/src/main/java/com/datageeks/limitsservice/controller/LimitsController.java
@@ -0,0 +1,23 @@
+package com.datageeks.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.datageeks.limitsservice.bean.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration = null;
+	
+	@GetMapping("/limits")
+	public ResponseEntity<Configuration> getConfigInfo()
+	{
+		return new ResponseEntity<Configuration>(configuration,HttpStatus.ACCEPTED);
+	}
+
+}
