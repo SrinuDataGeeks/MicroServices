8df0be7f63e4ee28c24334c03962b3a350365cc3
SrinuNp <SrinuNp111@gmail.com>
2019-11-04 10:52:53 +0530

    Wokring With Eureka Naming Server

--- Session5-RecapOfConcepts/google-map-service/src/main/java/com/datageeks/GoogleMapServiceApplication.java
+++ Session5-RecapOfConcepts/google-map-service/src/main/java/com/datageeks/GoogleMapServiceApplication.java
@@ -2,8 +2,10 @@ package com.datageeks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GoogleMapServiceApplication {
 
 	public static void main(String[] args) {
